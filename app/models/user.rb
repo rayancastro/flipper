56cc3c7645c7dc715adb7bc1510493f5d8697d84
rayancastro <rayancdc@gmail.com>
2019-03-10 16:53:21 -0300

    Add name and admin field to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  has_many :leads
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def admin?
+  	self.admin
+  end
 end
