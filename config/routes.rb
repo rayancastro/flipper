52450d8c1f8895a93b63a17f02d58544c95ea00d
rayancastro <rayancdc@gmail.com>
2019-03-10 19:44:36 -0300

    Add contacts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
 
   resources :leads
+  resources :contacts
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
