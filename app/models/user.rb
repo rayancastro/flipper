3107357797eda8d19dd3365294c4c254288696c3
rayancastro <rayancdc@gmail.com>
2019-03-14 21:51:45 -0300

    Add level_up and experience table

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,36 @@ class User < ApplicationRecord
   def admin?
   	self.admin
   end
+
+  def closer?
+    self.account_manager
+  end
+
+  def sdr?
+    !self.account_manager
+  end
+
+  def level_up
+    experience_table = User.experience_table
+    previous_level = self.level
+
+    while self.experience_points >= experience_table[self.level + 1]
+      self.level += 1
+    end
+    self.save
+    puts "User leveled up to level #{self.level}!" if self.level != previous_level
+  end
+
+  def self.experience_table
+    levels = {}
+    total_xp = 0
+    levels[1] = 0
+    2.upto(100) do |level|
+      level_up_ammount = 100 * (level - 1)
+      total_xp += level_up_ammount
+      levels[level] = total_xp
+    end
+    
+    levels
+  end
 end
